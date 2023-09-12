3f66df685d51ac363f28514adc6a5a4ce2bfb015
thejjwang <jonathanjacobwang@gmail.com>
2023-09-12 15:18:35 -0700

    setup

---
+++ app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+// import { ContextProvider } from './contexts/ContextProvider';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ContextProvider>
+//       <App />
+//     </ContextProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+);

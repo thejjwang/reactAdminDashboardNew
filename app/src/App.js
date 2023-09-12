cee9637431aef2a245b0f011fc810c9f03db6f2e
thejjwang <jonathanjacobwang@gmail.com>
2023-09-12 16:06:22 -0700

    no errors

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages'
 import "./App.css";
 
 const App = () => {
@@ -37,8 +37,8 @@ const App = () => {
           <div>
             <Routes>
                 {/* Dashboard */}
-                <Route path="/" element={<ECommerce />} />
-                <Route path="/ecommerce" element={<ECommerce />} />
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
 
                 {/* Pages */}
                 <Route path="/orders" element={<Orders />} />

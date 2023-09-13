228d4aeed210a4c3a161a0f8776e677fd63e74ab
thejjwang <jonathanjacobwang@gmail.com>
2023-09-12 18:26:04 -0700

    homepage/ecommerce

--- app/src/App.js
+++ app/src/App.js
@@ -60,7 +60,6 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
           <div>
             <Routes>
               {/* Dashboard */}
@@ -89,6 +88,7 @@ const App = () => {
               <Route path="/stacked" element={<Stacked />} />
             </Routes>
           </div>
+        </div>
         </div>
       </BrowserRouter>
     </div>
